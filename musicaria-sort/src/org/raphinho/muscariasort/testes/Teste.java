a01014e148949375799e2fdb7643c1e130b89d5e
Raphael Righetti <raphaelrighettiramos@hotmail.com>
2023-01-14 19:23:32 -0300

    atualizando anotacoes e adicionando lambdas ao musicaria-sort

--- musicaria-sort/src/org/raphinho/muscariasort/testes/Teste.java
+++ musicaria-sort/src/org/raphinho/muscariasort/testes/Teste.java
@@ -1,17 +1,27 @@
 package org.raphinho.muscariasort.testes;
 
-import org.raphinho.muscariasort.comparators.MusicoComparator;
 import org.raphinho.muscariasort.modelos.Musico;
 import org.raphinho.muscariasort.musicos.Baixista;
 import org.raphinho.muscariasort.musicos.Baterista;
 import org.raphinho.muscariasort.musicos.Guitarrista;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Teste {
 
     public static void main(String[] args) {
+//        Comparator<Musico> comparator = new Comparator<Musico>() {
+//            @Override
+//            public int compare(Musico m1, Musico m2) {
+//
+//                return Integer.compare(m1.getIdade(), m2.getIdade());
+//            }
+//        };
+
+        Comparator<Musico> comparator = (m1, m2) -> Integer.compare(m1.getIdade(), m2.getIdade());
+
         List<Musico> musicos = new ArrayList<>();
 
         Baixista raphael = new Baixista("Raphael", 20, false);
@@ -29,10 +39,16 @@ public class Teste {
         System.out.println("------------");
 
         // musicos.sort(null);
-        musicos.sort(new MusicoComparator());
+        // musicos.sort(new MusicoComparator());
+//        musicos.sort(new Comparator<Musico>() {
+//            @Override
+//            public int compare(Musico m1, Musico m2) {
+//                return Integer.compare(m1.getIdade(), m2.getIdade());
+//            }
+//        });
+//        musicos.sort(comparator);
+        musicos.sort((m1, m2) -> Integer.compare(m1.getIdade(), m2.getIdade()));
 
-        for (Musico musico : musicos) {
-            System.out.println(musico);
-        }
+        musicos.forEach((m) -> System.out.println(m));
     }
 }
