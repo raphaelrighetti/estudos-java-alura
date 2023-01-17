48770a057eda963d7717252eda8279452adbab42
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2023-01-17 15:42:30 -0300

    atualizando anotacoes e adicionando teste-fluxo-entrada-saida

---
+++ teste-fluxo-entrada-saida/src/org/raphinho/testefluxo/testes/Teste.java
@@ -0,0 +1,24 @@
+package org.raphinho.testefluxo.testes;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Teste {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("teste.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        while (linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+        br.close();
+    }
+}
